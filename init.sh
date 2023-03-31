efd5155833061c57b2940c988e687deaea47bb15
Alex Master <23503669+smolbytes@users.noreply.github.com>
2023-03-31 10:50:29 -0400

    updates

--- init.sh
+++ init.sh
@@ -9,20 +9,21 @@ apt -yq install --no-install-recommends coreutils curl grep ca-certificates gpg 
 curl https://pkg.cloudflareclient.com/pubkey.gpg | gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/cloudflare-client.list
 apt -q update
-apt -yq install cloudflare-warp
+apt -yq install cloudflare-warp  #no systemd in container, failures in apt log are normal
 #setup options
-warp-svc &
+nohup warp-svc &
 #warp-svc 1>/dev/null 2>/dev/null &
 sleep 10
 warp-cli --accept-tos register
 warp-cli --accept-tos set-mode proxy
 warp-cli --accept-tos set-proxy-port 9000
 warp-cli --accept-tos enable-always-on
-warp-cli --accept-tos connect
+#warp-cli --accept-tos connect #let Dockerfile handle
 
 #add routing rules to network interface
-nic_name=$(ip -br l | awk '$1 !~ "lo>|vir|wl" { print $1}')  #TODO make this work in docker also
-echo ${nic_name}
+#nic_name=$(ip -br l | awk '$1 !~ "lo>|vir|wl" { print $1}')  #TODO make this work in docker also
+nic_name='eth0'
+#echo ${nic_name}
 #sysctl -w net.ipv4.conf.${nic_name}.route_localnet=1   #needs to be done in docker compose file instead
 iptables -t nat -A PREROUTING -i ${nic_name} -p tcp --dport 9000 -j DNAT --to-destination 127.0.0.1:9000
 
